4f4e50c191999ffaae2a75dfe0ee04b4ce3305fe
Home-PC\Home <pakoh200@gmail.com>
2016-11-07 20:00:35 +0900

    외부 tomcat에 war 배포하도록 설정 추가함.

---
+++ src/main/java/com/howorld/MyWenInitializer.java
@@ -0,0 +1,11 @@
+package com.howorld;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class MyWenInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MyHoworldApplication.class);
+	}
+}
