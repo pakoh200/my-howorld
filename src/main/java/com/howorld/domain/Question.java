83f1c17246608ff2890437b7e3ade1fa244ca7f0
Home-PC\Home <pakoh200@gmail.com>
2016-11-08 19:26:45 +0900

    swagger 사용

--- src/main/java/com/howorld/domain/Question.java
+++ src/main/java/com/howorld/domain/Question.java
@@ -17,12 +17,7 @@ import javax.persistence.OrderBy;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
-public class Question {
-	@Id
-	@GeneratedValue
-	@JsonProperty
-	private Long id;
-	
+public class Question extends AbstractEntity {		
 	@ManyToOne
 	@JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
 	@JsonProperty
@@ -37,9 +32,7 @@ public class Question {
 	
 	@JsonProperty
 	private Integer countOfAnswer = 0;
-	
-	private LocalDateTime createDate;
-	
+		
 	@OneToMany(mappedBy="question")
 	@OrderBy("id DESC")
 	private List<Answer> answers;
@@ -50,15 +43,7 @@ public class Question {
 		super();
 		this.writer = writer;
 		this.title = title;
-		this.contents = contents;
-		this.createDate = LocalDateTime.now();
-	}
-	
-	public String getFormattedCreateDate(){
-		if(createDate == null){
-			return "";
-		}
-		return createDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
+		this.contents = contents;	
 	}
 
 	public void update(String title, String contents) {
@@ -77,31 +62,6 @@ public class Question {
 	
 	public void deleteAnswer(){
 		this.countOfAnswer -= 1;
-	}
+	}	
 	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Question other = (Question) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
 }
--- src/main/java/com/howorld/domain/User.java
+++ src/main/java/com/howorld/domain/User.java
@@ -9,12 +9,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity//database 연결 에너테이션
-public class User {
-	@Id//primary key
-	@GeneratedValue//자동증가
-	@JsonProperty
-	private Long id;
-	
+public class User extends AbstractEntity {
 	@Column(nullable=false, length=20, unique=true)
 	@JsonProperty
 	private String userId;
@@ -27,15 +22,7 @@ public class User {
 	
 	@JsonProperty
 	private String email;
-	
-	public boolean matchId(Long newId){
-		if(newId == null){
-			return false;
-		}
 		
-		return newId.equals(id);
-	}
-	
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
@@ -70,34 +57,17 @@ public class User {
 		this.email = newUser.email;
 	}	
 	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+	public boolean matchId(Long newId){
+		if(newId == null){
+			return false;
+		}
+		
+		return newId.equals(getId());
 	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
+	
 	@Override
 	public String toString() {
-		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+		return "User [" + super.toString() + ", userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
 	}
 
 }
