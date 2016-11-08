19d3f3436699ec4249ae6f5323687732ebee7760
Home-PC\Home <pakoh200@gmail.com>
2016-11-08 17:13:46 +0900

    ajax 답변 삭제 기능 구현

--- src/main/java/com/howorld/domain/Answer.java
+++ src/main/java/com/howorld/domain/Answer.java
@@ -52,6 +52,10 @@ public class Answer {
 		}
 		return createDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
 	}
+	
+	public boolean isSameWriter(User loginUser) {
+		return loginUser.equals(this.writer);
+	}	
 
 	@Override
 	public int hashCode() {
@@ -82,5 +86,5 @@ public class Answer {
 	public String toString() {
 		return "Answer [id=" + id + ", writer=" + writer + ", contents=" + contents + ", createDate=" + createDate
 				+ "]";
-	}	
+	}
 }
